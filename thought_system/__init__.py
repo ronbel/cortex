4856ec190c4dec7a8e2cfb6c928bc5ac736d2311
Ron Belkin <ronbelkin@mail.tau.ac.il>
2019-12-14 17:11:35 +0200

    Added webserver

--- thought_system/__init__.py
+++ thought_system/__init__.py
@@ -1,5 +1,6 @@
 from .thought import Thought
 from .client import upload_thought
 from .server import run_server
+from .website import run_webserver
 
 version = '0.0.1'
--- thought_system/website/webserver.py
+++ thought_system/website/webserver.py
@@ -1,1 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import pathlib
+
+
+def format_timestamp_name(name):
+    name = name.strip('.txt')
+    split_name = name.split('_')
+    split_name[1] = split_name[1].replace('-', ':')
+    return ' '.join(split_name)
+
+
+def run_webserver(address, data_path):
+    server = Flask(__name__)
+    data_dir = pathlib.Path(data_path)
+
+    @server.route('/')
+    def index():
+        user_list = [user_dir.name for user_dir in data_dir.iterdir()]
+        return render_template('index.html', users=user_list)
+
+    @server.route('/users/<int:id>')
+    def user_page(id):
+        user_path = data_dir / str(id)
+        thoughts = []
+        for file in user_path.iterdir():
+            thoughts.extend([{'timestamp': format_timestamp_name(file.name.strip('.txt')), 'content': line} for line in
+                             open(file).readlines()])
+        return render_template('user-page.html', thoughts=thoughts)
+
+    server.run(host=address[0], port=address[1], debug=True)
