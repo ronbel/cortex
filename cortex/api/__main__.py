78ed0aa2c21ffb0c9f5dab2d509c0ebb7dc4fa7d
Ron Belkin <ronbelkin@mail.tau.ac.il>
2020-03-02 23:30:37 +0200

    Added more routes to API

--- cortex/api/__main__.py
+++ cortex/api/__main__.py
@@ -11,9 +11,8 @@ def main():
 @click.option('-h', '--host')
 @click.option('-p', '--port')
 @click.option('-d', '--database')
-def run_api(host='localhost', port='5000',database='mongodb://localhost:27017'):
-    run_api_server(host=host,port=port,database_url=database)
-
+def run_api(host='localhost', port='5000', database='mongodb://localhost:27017'):
+    run_api_server(host=host, port=port, database_url=database)
 
 
 if __name__ == '__main__':
--- cortex/api/api_server.py
+++ cortex/api/api_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, make_response
 from pymongo import MongoClient
+from .config import get_config
 
 DB_NAME = 'cortex-db'
 
@@ -20,5 +21,21 @@ def run_api_server(host='localhost', port='5000', database_url='mongodb://localh
         if not result:
             return make_response('User not found\n', 404)
         return make_response(jsonify(result), 200)
+
+    @server.route('/users/<int:id>/snapshots')
+    def get_user_snapshots(id):
+        result = users.find_one({"user_id": str(id)}, {"snapshots": {
+                                "snapshot_id": 1, "datetime": 1}, "_id": 0})
+        if not result:
+            return make_response('User not found\n', 404)
+        return make_response(jsonify(result.snapshots), 200)
+    
+    @server.route('/users/<int:user_id>/snapshots/<int:snap_id>')
+    def get_snapshot(user_id,snap_id):
+        result = users.find_one({"user_id": str(user_id), "snapshots": {"snapshot_id": snap_id}}, {"_id": 0, "snapshots":1 })
+        if not result:
+            return make_response('Snapshot not found\n', 404)
+        return make_response(jsonify(result.snapshots[0]), 200)
+
 
     server.run(host=host, port=port)
