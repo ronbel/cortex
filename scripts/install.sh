fe4240be69eacf8263de3a03505b2913d12fa172
Ron Belkin <ronbelkin@mail.tau.ac.il>
2020-03-17 21:05:21 +0200

    Updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,10 +5,14 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
-    python -m virtualenv .env --prompt "[project-312350812] "
+    python -m virtualenv .env --prompt "[cortex] "
     find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     .env/bin/pip install -U pip
     .env/bin/pip install -r requirements.txt
+    sudo npm install -g @vue/cli
+    cd cortex/gui/cortex-gui
+    npm install
+    npm run build
 }
 
 
