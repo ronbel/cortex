b21771971518b6c78151f2ac2982a9a127e85bae
Ron Belkin <ronbelkin@mail.tau.ac.il>
2020-04-08 19:44:46 +0300

    Fixed requirements and conf.py for Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@ author = 'Ron Belkin'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.autohttp.flask', 'sphinx_click.ext', 'sphinx_rtd_theme']
+extensions = ['sphinx.ext.autodoc', 'sphinx_click.ext', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
